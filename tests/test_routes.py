6daf58a8e7238e396116a2a4ed67ef587e164cea
rashidwassan <rashidwassaan@gmail.com>
2023-08-22 18:42:57 -0400

    add method not allowed test

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -168,3 +168,8 @@ class TestAccountService(TestCase):
         account = self._create_accounts(1)[0]
         resp = self.client.delete(f"{BASE_URL}/{account.id}")
         self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_method_not_allowed(self):
+        """It should not allow an illegal method call"""
+        resp = self.client.delete(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
