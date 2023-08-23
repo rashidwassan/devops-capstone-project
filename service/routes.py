bedb372791b3f989491f834a4515cbde3492f295
rashidwassan <rashidwassaan@gmail.com>
2023-08-23 08:56:06 -0400

    fix flake8 error

--- service/routes.py
+++ service/routes.py
@@ -111,8 +111,7 @@ def update_accounts(account_id):
 
     account = Account.find(account_id)
     if not account:
-        abort(status.HTTP_404_NOT_FOUND,
-            f"Account with id [{account_id}] could not be found.")
+        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
 
     account.deserialize(request.get_json())
     account.update()
