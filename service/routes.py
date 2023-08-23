b0d7bb362aa46039b1b8ab0aac0169c406861849
rashidwassan <rashidwassaan@gmail.com>
2023-08-23 08:42:48 -0400

    Added badge for GitHub Actions

--- service/routes.py
+++ service/routes.py
@@ -60,6 +60,8 @@ def create_accounts():
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
+
+
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
@@ -98,6 +100,7 @@ def get_accounts(account_id):
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
 
+
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
     """
@@ -109,7 +112,7 @@ def update_accounts(account_id):
     account = Account.find(account_id)
     if not account:
         abort(status.HTTP_404_NOT_FOUND,
-                f"Account with id [{account_id}] could not be found.")
+            f"Account with id [{account_id}] could not be found.")
 
     account.deserialize(request.get_json())
     account.update()
@@ -119,6 +122,7 @@ def update_accounts(account_id):
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
+
 
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_accounts(account_id):
